91feee050b0231f4bcc8c7ac654f281bec03b68a
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2022-12-12 15:44:43 +0100

    npc rotation

---
+++ Assets/Lisa's Folder (No Touchy)/NpcRotation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcRotation : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    void Update()
+    {
+        transform.forward = new Vector3(player.transform.forward.x, 0, player.transform.forward.z);
+    }
+}
