cf9782ee427b18efdc15114c7aecb276205df97b
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2022-12-12 19:51:17 +0100

    microphone input and output

---
+++ Assets/Lisa's Folder (No Touchy)/MicStuff.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicStuff : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            audioSource.clip = Microphone.Start("", true, 100, 44100);
+            
+            while (Microphone.GetPosition("") <= 0)
+            {
+            }
+
+            audioSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Microphone.End("");
+        }
+    }
+}
