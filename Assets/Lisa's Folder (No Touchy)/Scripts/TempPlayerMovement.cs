900d77deca86ff45c8a8a1ec00a2e7b853c1c423
Lisa Olsson <97893739+LisaOlsson2@users.noreply.github.com>
2022-12-16 11:42:20 +0100

    characters

--- Assets/Lisa's Folder (No Touchy)/Scripts/TempPlayerMovement.cs
+++ Assets/Lisa's Folder (No Touchy)/Scripts/TempPlayerMovement.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class TempPlayerMovement : MonoBehaviour
 {
+    readonly float speed = 15;
+
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-5, 0, 0) * Time.deltaTime;
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(5, 0, 0) * Time.deltaTime;
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 0, 5) * Time.deltaTime;
+            transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, 0, -5) * Time.deltaTime;
+            transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
         }
     }
 }
